d633589139ac0364cde4bef35922c111942746d0
marcos <marcseb.160900@gmail.com>
2024-10-28 19:49:04 -0400

    aumente a tipo de ejercicio

--- resources/views/TypeExercises/index.blade.php
+++ resources/views/TypeExercises/index.blade.php
@@ -28,6 +28,84 @@
         </div>
     </div>
 @stop
+@section('content')
+
+    <div class="content">
+        <div class="clearfix"></div>
+        <div class="clearfix"></div>
+        <div class="card">
+            <div class="card-header">
+                <div class="d-flex justify-content-end">
+                    <div class="btn-group mb-3" role="group">
+                        <a href="#" class="btn btn-danger btn-lg">
+                            <i class="fas fa-file-pdf"></i> PDF
+                        </a>
+                        <a href="#" class="btn btn-success btn-lg">
+                            <i class="fas fa-file-excel"></i> Excel
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <div class="card">
+            <div class="card-header">
+                <ul class="nav nav-tabs align-items-end card-header-tabs w-100">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="{!! url()->current() !!}"><i class="fa fa-list mr-2"></i>Lista
+                            de Ejercicios</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{!! route('exercise-types.create') !!}"><i class="fa fa-plus mr-2"></i>Crear
+                            Ejercicio</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="container">
+                <h2 class="my-4">Lista de Ejercicios</h2>
+                <div class="row">
+                    @foreach ($ejercicios as $ejercicio)
+                        <div class="col-md-15 mb-2"> <!-- Card más ancha ocupando 6 columnas -->
+                            <div class="card h-100 shadow-lg"
+                                style="display: flex; flex-direction: row; max-height: 200px;">
+                                <img src="#" class="card-img-left" alt="Imagen del Ejercicio"
+                                    style="width: 40%; object-fit: cover;">
+                                <div class="card-body" style="flex: 1;">
+                                    <h5 class="card-title">{{ $ejercicio->nombre }}</h5>
+                                    <p class="card-text">{{ $ejercicio->descripcion }}</p>
+                                    <p class="card-text">
+                                        @if ($ejercicio->estado == 'inactivo')
+                                            <span class="badge bg-danger">{{ $ejercicio->estado }}</span>
+                                        @else
+                                            <span class="badge bg-success">{{ $ejercicio->estado }}</span>
+                                        @endif
+                                    </p>
+                                    <p>
+
+                                        <button type="button" class="btn btn-danger btn-sm delete-exercise-types"
+                                            data-id="{{ $ejercicio->id }}" title="Eliminar">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </button>
+
+                                    </p>
+                                    <a href="{{ route('exercise-types.edit', $ejercicio->id) }}"
+                                        class="btn btn-warning mt-2" title="Editar">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <a href="#" class="btn btn-primary mt-2">Ver
+                                        Detalles</a>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
 
 @section('content')
 
